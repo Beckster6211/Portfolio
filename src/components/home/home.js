e4957c8a43c3721212114e142a634d811093d608
= <beckyljones151192@gmail.com>
2021-02-17 12:57:22 +0000

    added nicknames to home screen

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-import Header from "../header/header";
+import Names from "../names/names";
 
 function Home() {
   return (
@@ -12,6 +12,13 @@ function Home() {
         alt="Me"
       />
       <img class="picture" src="./Capture.PNG" alt="SOC avatar" />
+      <h3 class="heading">Me</h3>
+      <p class="text">
+        My full name is Rebecca Louise Jones, I prefer to be called Becky. The
+        only times I am Rebecca is for offical reasons or by people that don't
+        know me...(or if I am in trouble).
+      </p>
+      <Names />
     </div>
   );
 }
---
+++ src/components/names/names.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Names() {
+  return (
+    <div>
+      <h3 class="heading">Nicknames:</h3>
+      <p class="subHeading">I have a few nicknames in my family:</p>
+      <div class="subHeading">
+        <details>
+          <summary>Beckster </summary>
+          <p class="text">
+            The story of Beckster, is from my cousin, as he was growing up had a
+            speech impediment and couldn't say 'Becky', not sure why it's quite
+            easy... so he would call me 'Becks', that got morphed into
+            'Beckster' its actually got morphed more than that to the point
+            where my uncle has been know to call 'Sophie' and I've answered.
+            (Sophie Ellis-Bextor, thats okay I'm not going to murder him on the
+            dancefloor).
+          </p>
+        </details>
+        <details>
+          <summary>NintendoGirl</summary>
+          <p class="text">
+            I have always loved Nintendo products prefer handheld consoles to on
+            the television, I have two brothers who are gamers one older (who
+            favours PlayStation) and one younger (who favours Xbox), they have
+            conversations about graphics and processors etc and I just nod
+            along. (They always laugh that my responseis 'I like Mario').
+            <br></br>
+            <br></br>
+            The story behind the name Nintendogirl is actually from the security
+            guard at the Queen Elizabeth Hospital Cancer Care building. My dad
+            had prostrate cancer and had to undergo radiotheraphy as his
+            treatment, this involved being at the QE everyday for weeks. To keep
+            myself entertained during this time I was playing my 3DS the
+            security guard every morning (during his rounds) would stop and chat
+            with me about games. During my dads treatment it was my birthday and
+            the whole family got me a Nintendo Switch (something I had been
+            talking about saving for). So for the remainder of my dads treatment
+            I would be playing my Nintendo switch. The security guard would
+            still stop during his rounds and we talk about the games I could get
+            and he would even have a Mario Kart race with me (he was quite
+            good).
+            <br></br>
+            <br></br>
+            He would greet me every morning walking into the building with
+            'Morning Nintendogirl' and I would response with 'Morning Security
+            guard' (I wish I knew his name).
+          </p>
+        </details>
+        <details>
+          <summary>B</summary>
+          <p class="text">
+            Sometimes my older brother just simplifies my name to just B. I
+            still respond.
+          </p>
+        </details>
+        <details>
+          <summary>Auntie Becca</summary>
+          <p class="text">
+            My nephew can't talk yet but maybe auntie Becca (think that would be
+            easy for baby learning to speak...).
+          </p>
+        </details>
+      </div>
+    </div>
+  );
+}
+export default Names;
